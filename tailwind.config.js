612c441b71a9330163fbc3daa46df9fde61a7f72
wahyubudies <wahyubudies1@gmail.com>
2022-06-03 19:49:16 +0700

    init push to github for first time

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': 'Poppins, Arial, sans-serif'
+      },
+      colors: {
+        black: '#2C2C2C',
+        primary: '#178847',
+        secondary: '#F5F6FA'
+      },
+      backgroundImage: {
+        'hero-pattern': 'url("img/hero-pattern.jpg")'
+      }
+    }
+  },
+  plugins: [],
+}
